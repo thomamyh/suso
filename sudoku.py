f0a15737e71a7218471afbdf94582b8bc8734c68
Thomas Myhre <thomassmy@gmail.com>
2021-09-15 20:14:28 +0200

    Added the function to read the board from a file.

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,19 @@
+BOARD_FILE = "board2.txt"
+
 def read_board():
-    # Reads the board from a file
-    pass
+    # Reads the board from a file as a string and splits it up
+    # into a list of lists with each row being a list.
+    with open(BOARD_FILE, 'r') as f:
+        from_file = f.read()
+    
+    from_file = from_file.split("\n")
+    board = []
+
+    for row in from_file:
+        r = [int(i) for i in row]
+        board.append(r)
+    
+    return board
 
 def solve(board):
     # Solves the sudoku puzzle
@@ -16,6 +29,7 @@ def get_empty():
 
 def main():
     board = read_board()
+    print(board)
 
 if __name__ == "__main__":
     main()
