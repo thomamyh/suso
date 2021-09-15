5c4c4600500ac757a81eae60c1d0d5ef1b974566
Thomas Myhre <thomassmy@gmail.com>
2021-09-15 20:01:57 +0200

    Added a description to README.md and started writing the functions in the main file.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# suso
+# Sudoku Solver
+
+#### Reads in a n by n sudoku board from a file and solves the puzzle
---
+++ sudoku.py
@@ -0,0 +1,21 @@
+def read_board():
+    # Reads the board from a file
+    pass
+
+def solve(board):
+    # Solves the sudoku puzzle
+    pass
+
+def possible():
+    # Checks if the value can be entered in the square
+    pass
+
+def get_empty():
+    # Returns the next empty square on the board
+    pass
+
+def main():
+    board = read_board()
+
+if __name__ == "__main__":
+    main()
